2146af35c857f86d63acf4a38711c7739c6f2195
ricardo <ricardocancar@gmail.com>
2024-11-01 20:28:11 +0100

    trading: add redis for cache data

--- app/api/routes/indicators.py
+++ app/api/routes/indicators.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+import json
+from fastapi import APIRouter, Request
+import pandas as pd
 from trading.indicators.moving_averages import MovingAverages
 import yfinance as yf
 
@@ -7,7 +9,7 @@ router = APIRouter()
 
 
 @router.get("/sma/{symbol}")
-async def get_sma(symbol: str, short_period: int = 20, long_period: int = 50, span_time:str='1y', interval:str='1d') -> list[dict]:
+async def get_sma(symbol: str, request: Request, short_period: int = 20, long_period: int = 50, span_time:str='1y', interval:str='1d') -> list[dict]:
     """
     Get Simple Moving Average (SMA) for a given stock symbol.
 
@@ -17,11 +19,24 @@ async def get_sma(symbol: str, short_period: int = 20, long_period: int = 50, sp
     :param span_time: Time span for the data (default is '1y' for one year)
     :param interval: Time interval between data points (default is '1d' for daily)
     """
-    stock = yf.Ticker(symbol)
-    data = stock.history(period=span_time, interval=interval)
+    redis_client = request.app.state.redis
+    key = f"{span_time}:{interval}:{symbol}:{short_period}:{long_period}"
+    result = await redis_client.get(key)
+    if result is not None:
+        # TODO: transform string into list of dict
+        return result
+    key = key = f"{span_time}:{interval}:{symbol}"
+    result = await redis_client.get(key)
+    if result is None:
+        stock = yf.Ticker(symbol)
+        data = stock.history(period=span_time, interval=interval)
+        await redis_client.set(key,str(data.to_dict(orient="records")))
+    else:
+        data = pd.Dataframe(result)
     long_sma = MovingAverages(data=data['Close']).sma(long_period)
     short_sma = MovingAverages(data=data['Close']).sma(short_period)
     data['sma_short'] = short_sma
     data['sma_long'] = long_sma
-
+    key = f"{span_time}:{interval}:{symbol}:{short_period}:{long_period}"
+    await redis_client.set(key, str(data.reset_index().to_dict(orient="records")))
     return data.reset_index().to_dict(orient="records")
