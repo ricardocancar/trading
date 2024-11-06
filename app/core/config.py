025de43b94f26ffdec7496f468619ea1e89e243f
ricardo <ricardocancar@gmail.com>
2024-11-06 21:33:53 +0100

    trading: add redis to docker compose

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "My FastAPI Project"
     ALLOWED_HOSTS: list = ["*"]  # Deberías especificar los orígenes permitidos en producción
     # DATABASE_URL: str = "sqlite:///./test.db"
-    redis_host: str = os.getenv("REDIS_HOST", "localhost")
+    redis_host: str = os.getenv("REDIS_HOST", "redis")
     redis_port: int = int(os.getenv("REDIS_PORT", 6379))
     redis_db: int = int(os.getenv("REDIS_DB", 0))
     redis_password: str = os.getenv("REDIS_PASSWORD", "")
